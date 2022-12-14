1db8d79d4d8b6351d5b7aa6d2b96e3c150e58511
luankofs <luan.luan2009@hotmail.com>
2022-12-14 13:27:17 -0300

    JS
    js

---
+++ ProgBR/Ex02 - Calc entre idades/calc.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo a nossa calculadora de idades!")
+let NameOldAge = prompt("Insira o Nome da pessoa mais velha: ")
+let OldAge = prompt("Insira a idade da pessoa mais velha")
+let NameYoungAge = prompt("Insira o nome da pessoa mais nova")
+let YoungAge = prompt("Insira a idade da pessoa mais nova")
+let DiferenceAge = (OldAge - YoungAge)
+alert("Nome da pessoa mais velha: "+ NameOldAge + "\nNome pessoa mais nova: "+ NameYoungAge + "\nDiferença de idade: "+ DiferenceAge +" anos")
